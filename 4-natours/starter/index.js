de3e89d6c99547ac76c7290675c34f265b58778a
Tu <52100138@student.tdtu.edu.vn>
2024-09-04 13:02:21 +0700

    Update

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -10,16 +10,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'success', results: tours.length, data: { tours } });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const length = tours.length;
-  const newTour = { id: tours[length - 1].id + 1, ...req.body };
-  tours.push(newTour);
+const writeFile = (tours, newTour) => {
   fs.writeFileSync(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -32,6 +23,68 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+};
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const length = tours.length;
+  const newTour = { id: tours[length - 1].id + 1, ...req.body };
+  tours.push(newTour);
+  writeFile(tours, newTour);
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.get('/api/v2/tours', (req, res) => {
+  const { a, b } = req.query;
+  console.log(a, b);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      a,
+      b,
+    },
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(200).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+  }
+
+  const { duration } = req.body;
+
+  const newTour = { ...tour, duration };
+  console.log(newTour);
+  const listTour = tours.filter((el) => el.id !== id);
+  const newListTour = [...listTour, newTour];
+  writeFile(newListTour, newTour);
 });
 
 app.listen(port, () => {
